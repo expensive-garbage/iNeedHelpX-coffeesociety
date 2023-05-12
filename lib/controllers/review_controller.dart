c8d57fd3fce53b4c82cf3f711fdfe0ae1e389682
Julia Pak <jpak0444@gmail.com>
2023-05-12 16:00:24 -0400

    review controller

---
+++ lib/controllers/review_controller.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ReviewController extends GetxController {
+  static ReviewController instance = Get.find();
+  // int? index = 0;
+
+  @override
+  void onInit() async {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+}
